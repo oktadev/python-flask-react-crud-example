d6aaea1e86f3d003c06f6d879f2ebcfa38ffd8d4
Matt Raible <matt.raible@okta.com>
2021-04-05 10:45:39 -0600

    Re-create with latest Create React App

--- app/http/web/app/src/githubClient.js
+++
@@ -1,5 +0,0 @@
-function getJSONRepos(query) {
-  return fetch('https://api.github.com/search/repositories?q=' + query).then(response => response.json());
-}
-
-export default getJSONRepos;
--- app/http/web/app/src/index.js
+++ app/http/web/app/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom'
-import { createBrowserHistory } from 'history'
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import Main from './Main';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const history = createBrowserHistory();
-
-ReactDOM.render((
-  <Router history={history}>
-    <Main history={history} />
-  </Router>
-), document.getElementById('root'))
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
